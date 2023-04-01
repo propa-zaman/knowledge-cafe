d649b996d895794ebfab66b2c3a33d73a6d0340d
Afra <propazaman12@gmail.com>
2023-04-01 13:07:55 +0600

    fontawsome installed

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 import './Post.css'
 
 const Post = (props) => {
@@ -10,13 +12,14 @@ const Post = (props) => {
             <div className='author'>
             <img src={images.author_image} alt="" className='author-image'/>
             <p>{author}</p>
-            <p className='read-time'>{read_time}</p>
+            <p className='read-time'>{read_time}<FontAwesomeIcon icon={faBookmark} /></p>
             </div>
             
             
             <p>{publish_date}</p>
             <h2>{title}</h2>
-            <button className='btn-mark' onClick={() => handleSpendTime(props.blog)}>Mark as read</button>
+            <button className='btn-mark' onClick={() => handleSpendTime(props.blog)}>Mark as read
+            </button>
             
         </div>
     );
